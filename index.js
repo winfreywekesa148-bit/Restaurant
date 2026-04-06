f898101c0204666d766823eba9fedb430f77baa4
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-04-06 16:28:36 +0300

    Changes on the featured drink and new burgers

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const featuredDrink = ["Strawberry Milkshake"]
 // function for burger
 function addBurger(){
    function newBurger() {
-    let newBurger = 'Flatburger'
-    burgers.push(newBurger)
+    let newBurger = 'Flatburger';
+    burgers.push(newBurger);
 
-   }
+   } newBurger()
+}
+
+// if statement
+if (true) {
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger)
+    console.log(burgers)
+
+    function changeFeaturedDrink() {
+         let featuredDrink = 'The JavaShake';
+        console.log(featuredDrink)
+    }
 }
