a5599fc92b7612056e1e623661f5771d6382b679
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-04-06 16:00:12 +0300

    addition of the new burger

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // array for burgers and milkshake
 const burgers = ["Hamburger", "Cheeseburger"]
 const featuredDrink = ["Strawberry Milkshake"]
+
+// function for burger
+function addBurger(){
+   function newBurger() {
+    let newBurger = 'Flatburger'
+    burgers.push(newBurger)
+
+   }
+}
